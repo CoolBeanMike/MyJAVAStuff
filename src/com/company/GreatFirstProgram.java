39bbbc3bb8be6a8621b72846fc068aa89007133d
CoolBeanMike <mikebova@verizon.net>
2016-09-12 00:02:55 -0400

    my first script

---
+++ src/com/company/GreatFirstProgram.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by mbova on 9/11/2016.
+ */
+
+public class GreatFirstProgram
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello world!");
+		System.out.println("Hello again");
+		System.out.println("coolbean");
+		
+	}
+}

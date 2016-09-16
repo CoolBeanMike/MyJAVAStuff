00ccd35707d7b90528dc915698f896dd354e99ab
CoolBeanMike <mikebova@verizon.net>
2016-09-15 23:24:23 -0400

    my first script

--- src/com/company/my_first_java_script.java
+++ src/com/company/my_first_java_script.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class my_first_java_script
 {
-	public static void main(String[] args)	{
+	public static void main(String[] args)
+	{
 		System.out.println("My cat is a cool dude and dude'et");
 	}
 }

ea6b8d01ff44f83919a1e07715d0c38757a8f2d0
CoolBeanMike <mikebova@verizon.net>
2016-09-16 00:02:37 -0400

    my first script

---
+++ src/com/company/NumbersAndMath.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by mbova on 9/15/2016.
+ */
+
+public class NumbersAndMath
+{
+    public static void main(String[] args)
+    {
+     System.out.println("I have my chickens to count now");
+     System.out.println("Hens " + (25 + 30 / 6));
+
+    }
+}

cf70399359c5ab9afee293857b7f8deea65b1e47
CoolBeanMike <mikebova@verizon.net>
2016-09-15 23:15:15 -0400

    my first script

--- src/com/company/GreatFirstProgram.java
+++ src/com/company/GreatFirstProgram.java
@@ -11,6 +11,7 @@ public class GreatFirstProgram
 		System.out.println("Hello world!");
 		System.out.println("Hello again");
 		System.out.println("coolbean");
-		
+		System.out.println("I am a cool dude \"said\" do you understand");
+		System.out.println("This is a 'cool bean' test .");
 	}
 }

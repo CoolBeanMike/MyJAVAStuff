a888560aa94abf00d075ecd7b28f21cd2adb7607
CoolBeanMike <mikebova@verizon.net>
2016-09-17 14:32:01 -0400

    my first script

--- src/com/company/NumbersAndMath.java
+++ src/com/company/NumbersAndMath.java
@@ -6,10 +6,17 @@ package com.company;
 
 public class NumbersAndMath
 {
-    public static void main(String[] args)
-    {
-     System.out.println("I have my chickens to count now");
-     System.out.println("Hens " + (25 + 30 / 6));
-
-    }
+	public static void main(String[] args)
+	{
+		System.out.println("I have my chickens to count now");
+		System.out.println("Hens " + (25 + 30 / 6));
+		System.out.println("Roosters: " + ((5+6+6)/10));
+		System.out.println("Now I will count the egs in total....");
+		System.out.println(10+11+12);
+		System.out.println("Is it true that 5+6 is > then 9+9");
+		System.out.println(5+6 > 9+9); //This will show a False
+		System.out.println("What is 3 + 2? ---> "  + (3 + 2));
+		System.out.println("What is 5 - 7? ---> "  + (5 - 7));
+		System.out.println("So what do we have here??? Is 3 >= (5+6) True or False " + ( 11 >= (5+6)));
+	}
 }

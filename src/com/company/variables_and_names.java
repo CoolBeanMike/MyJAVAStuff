0d647250646fb6106b00926e70dbddffd611f41c
CoolBeanMike <mikebova@verizon.net>
2016-09-17 15:16:52 -0400

    my first script

---
+++ src/com/company/variables_and_names.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by mbova on 9/17/2016.
+ */
+public class variables_and_names
+{
+	public static void main(String[] args)
+	{
+		int cars, drivers, pasengers, cars_not_driven, cars_driven;
+		double space_in_car, carpool_capacity, average_passengers_per_car;
+	}
+}
